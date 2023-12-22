347c1850eaa152bbe450fdf2ee438052c4f8fe25
Scott Flaherty <scott.d.flaherty@gmail.com>
2023-12-21 19:35:01 -0600

    initial commit

--- chef_server.tf
+++ chef_server.tf
@@ -1,53 +1,38 @@
-# Create resource group that will be used with Jenkins deploy
-resource "azurerm_resource_group" "jenkins" {
-  name     = "${var.resource_group_name}"
+#### CHEF ####
+# Create resource group that will be used with chef deploy
+resource "azurerm_resource_group" "chef" {
+  name     = "${var.chef_resource_group_name}"
   location = "${var.location}"
 }
 
 # Create public IPs
-resource "azurerm_public_ip" "jenkins" {
-    name                         = "${var.computer_name}-pubip"
-    location                     = "${azurerm_resource_group.jenkins.location}"
-    resource_group_name          = "${azurerm_resource_group.jenkins.name}"
+resource "azurerm_public_ip" "chef" {
+    name                         = "${var.chef_computer_name}-pubip"
+    location                     = "${azurerm_resource_group.chef.location}"
+    resource_group_name          = "${azurerm_resource_group.chef.name}"
     public_ip_address_allocation = "static"
 }
 
-# Create virtual NIC that will be used with our Jenkins instance.
-resource "azurerm_network_interface" "jenkins" {
-  name                = "${azurerm_resource_group.jenkins.name}-nic"
+# Create virtual NIC that will be used with our chef instance.
+resource "azurerm_network_interface" "chef" {
+  name                = "${azurerm_resource_group.chef.name}-nic"
   location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.jenkins.name}"
+  resource_group_name = "${azurerm_resource_group.chef.name}"
 
   ip_configuration {
-    name                          = "${var.computer_name}-ipconf"
+    name                          = "${var.chef_computer_name}-ipconf"
     subnet_id                     = "${var.subnet_id}"
     private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = "${element(azurerm_public_ip.jenkins.*.id, count.index)}"
+    public_ip_address_id          = "${element(azurerm_public_ip.chef.*.id, count.index)}"
   }
 }
 
-# STORAGE =======================================
-# resource "azurerm_storage_account" "test" {
-#   name                     = "${var.storageaccount}"
-#   resource_group_name      = "${azurerm_resource_group.jenkins.name}"
-#   location                 = "${var.location}"
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
-
-# resource "azurerm_storage_container" "test" {
-#   name                  = "vhds"
-#   resource_group_name   = "${azurerm_resource_group.jenkins.name}"
-#   storage_account_name  = "${azurerm_storage_account.test.name}"
-#   container_access_type = "private"
-# }
-
-# VIRTUAL MACHINE ================================
-resource "azurerm_virtual_machine" "chef_server" {
-  name                  = "${var.computer_name}"
+# VIRTUAL MACHINE 
+resource "azurerm_virtual_machine" "chef" {
+  name                  = "${var.chef_computer_name}"
   location              = "${var.location}"
-  resource_group_name   = "${azurerm_resource_group.jenkins.name}"
-  network_interface_ids = ["${azurerm_network_interface.jenkins.id}"]
+  resource_group_name   = "${azurerm_resource_group.chef.name}"
+  network_interface_ids = ["${azurerm_network_interface.chef.id}"]
   vm_size               = "${var.vm_size}"
 
   storage_image_reference {
@@ -58,51 +43,35 @@ resource "azurerm_virtual_machine" "chef_server" {
   }
 
   storage_os_disk {
-    name          = "${var.computer_name}-osdisk"
+    name          = "${var.chef_computer_name}-osdisk"
     caching       = "ReadWrite"
     create_option = "FromImage"
     managed_disk_type = "Standard_LRS"
     disk_size_gb      = "100"
     os_type           = "linux"
   }
-  # storage_os_disk {
-  #   name          = "${var.computer_name}-osdisk"
-  #   vhd_uri       = "${azurerm_storage_account.test.primary_blob_endpoint}${azurerm_storage_container.test.name}/${var.computer_name}-osdisk.vhd"
-  #   caching       = "ReadWrite"
-  #   create_option = "FromImage"
-  # }
 
   os_profile {
-    computer_name  = "${var.computer_name}"
-    admin_username = "${var.admin_user}"
-    admin_password = "${var.admin_password}"
+    computer_name  = "${var.chef_computer_name}"
+    admin_username = "${var.chef_admin_user}"
+    admin_password = "${var.chef_admin_password}"
   }
   
   os_profile_linux_config {
     disable_password_authentication = false
       ssh_keys {
-        path = "/home/${var.admin_user}/.ssh/authorized_keys"
+        path = "/home/${var.chef_admin_user}/.ssh/authorized_keys"
         key_data = "${file("~/.ssh/id_rsa.pub")}"
     }
   }
 
   connection {
       type = "ssh"
-      host = "${element(azurerm_public_ip.jenkins.*.ip_address, count.index)}"
-      user = "${var.admin_user}"
+      host = "${element(azurerm_public_ip.chef.*.ip_address, count.index)}"
+      user = "${var.chef_admin_user}"
       # password = "${var.admin_password}"
       private_key = "${file("~/.ssh/id_rsa")}"
       agent = false
-  }
-
-  provisioner "local-exec" {
-       command = "echo 'sleeping'"
-  }
-  provisioner "local-exec" {
-       command = "sleep 220"
-  }
-  provisioner "local-exec" {
-       command = "echo 'done sleeping'"
   }
   provisioner "file" {
     source      = "json/stuff_and_things.json"
@@ -127,3 +96,116 @@ resource "azurerm_virtual_machine" "chef_server" {
     ]
   }
 }
+# #### AUTOMATE ####
+# Create resource group that will be used with automate deploy
+resource "azurerm_resource_group" "automate" {
+  name     = "${var.auto_resource_group_name}"
+  location = "${var.location}"
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "automate" {
+    name                         = "${var.auto_computer_name}-pubip"
+    location                     = "${azurerm_resource_group.automate.location}"
+    resource_group_name          = "${azurerm_resource_group.automate.name}"
+    public_ip_address_allocation = "static"
+}
+
+# Create virtual NIC that will be used with our automate instance.
+resource "azurerm_network_interface" "automate" {
+  name                = "${azurerm_resource_group.automate.name}-nic"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.automate.name}"
+
+  ip_configuration {
+    name                          = "${var.auto_computer_name}-ipconf"
+    subnet_id                     = "${var.subnet_id}"
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = "${element(azurerm_public_ip.automate.*.id, count.index)}"
+  }
+}
+
+# VIRTUAL MACHINE 
+resource "azurerm_virtual_machine" "automate" {
+  name                  = "${var.auto_computer_name}"
+  location              = "${var.location}"
+  resource_group_name   = "${azurerm_resource_group.automate.name}"
+  network_interface_ids = ["${azurerm_network_interface.automate.id}"]
+  vm_size               = "${var.vm_size}"
+
+  storage_image_reference {
+    publisher = "${var.publisher}"
+    offer     = "${var.offer}"
+    sku       = "${var.sku}"
+    version   = "${var.version}"
+  }
+
+  storage_os_disk {
+    name          = "${var.auto_computer_name}-osdisk"
+    caching       = "ReadWrite"
+    create_option = "FromImage"
+    managed_disk_type = "Standard_LRS"
+    disk_size_gb      = "100"
+    os_type           = "linux"
+  }
+
+  os_profile {
+    computer_name  = "${var.auto_computer_name}"
+    admin_username = "${var.auto_admin_user}"
+    admin_password = "${var.auto_admin_password}"
+  }
+  
+  os_profile_linux_config {
+    disable_password_authentication = false
+      ssh_keys {
+        path = "/home/${var.auto_admin_user}/.ssh/authorized_keys"
+        key_data = "${file("~/.ssh/id_rsa.pub")}"
+    }
+  }
+
+  connection {
+      type = "ssh"
+      host = "${element(azurerm_public_ip.automate.*.ip_address, count.index)}"
+      user = "${var.auto_admin_user}"
+      # password = "${var.admin_password}"
+      private_key = "${file("~/.ssh/id_rsa")}"
+      agent = false
+  }
+
+  provisioner "local-exec" {
+       command = "echo 'sleeping'"
+  }
+  provisioner "local-exec" {
+       command = "sleep 220"
+  }
+  provisioner "local-exec" {
+       command = "echo 'done sleeping'"
+  }
+  provisioner "file" {
+    source      = "json/stuff_and_things.json"
+    destination = "/tmp/stuff_and_things.json"
+  }
+  # provisioner "local-exec" {
+  #      command = "sudo mkdir -p /drop"
+  # }
+  # provisioner "local-exec" {
+  #      command = "sudo scp ${var.auto_admin_user}@${azurerm_virtual_machine.chef.private_ip}:/drop/delivery.pem /tmp/delivery_user.pem"
+  # }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install git -y",
+      "sudo curl --connect-timeout 60 -L https://www.chef.io/chef/install.sh | sudo bash",
+      "sudo mkdir -p /var/chef/cache /var/chef/cookbooks",
+      "sudo git clone https://scott_flaherty:Grad_2155@bitbucket.org/trekbikes/cb_dvo_cautomate.git /var/chef/cookbooks/cb_dvo_cautomate",
+      "sudo wget -qO- https://supermarket.chef.io/cookbooks/ohai/versions/5.2.0/download |sudo tar xvzC /var/chef/cookbooks",
+      "sudo wget -qO- https://supermarket.chef.io/cookbooks/sysctl/versions/0.10.2/download |sudo tar xvzC /var/chef/cookbooks",
+      # "sudo scp $CHEF_USER@$CHEF_SERVER:/drop/delivery.pem /tmp/delivery_user.pem"
+      "sudo mv /var/chef/cookbooks/cb_dvo_cautomate/data_bags /var/chef/",
+      "sudo mv /tmp/stuff_and_things.json /var/chef/cache/stuff_and_things.json",
+      "sudo chef-solo -j '/var/chef/cache/stuff_and_things.json' -o 'recipe[cb_dvo_cautomate::default]'"
+    ]
+  }
+  depends_on = ["azurerm_virtual_machine.chef"]
+}
+
--- variables.tf
+++ variables.tf
@@ -1,52 +1,47 @@
-variable "resource_group_name" {
-  default = "AZL-ChefServer-02-tst"
-}
-variable "storageaccount" {
-  default = "chef2storagetst"
-}
 variable "location" {
   default = "eastus2"
 }
-# variable "publisher" {
-#   default = "OpenLogic"
-# }
 variable "publisher" {
   default = "Canonical"
 }
-# variable "offer" {
-#   default = "CentOS"
-# }
 variable "offer" {
   default = "UbuntuServer"
 }
-# variable "sku" {
-#   default = "7.3"
-# }
 variable "sku" {
   default = "16.04-LTS"
-}
-variable "admin_password" {
-  default = "ZyADTVd64swkdvfHFMeR"
-}
-variable "admin_user" {
-  default = "devops"
 }
 variable "version" {
   default = "latest"
 }
-variable "computer_name" {
-  default = "azl-chef-02-tst"
-}
 variable "vm_size" {
   default = "Standard_A2"
 }
-# this can be found using resource explorer in hte azure portal.  This one is Int-Mgmt
 variable "subnet_id" {
     default = "/subscriptions/9fbf7025-df40-4908-b7fb-a3a2144cee91/resourceGroups/AZ-RG-Network/providers/Microsoft.Network/virtualNetworks/AZ-VN-EastUS2-02/subnets/AZ-SN-dvo"
 }
-variable "ssh_key_thumbprint" {
-	default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDjSq6wA1WDmJnBEaiTJUk2wrqZ7OaeDR6VQ+xV5iShotocGzmgIuZSWNnG/eakio9V0dlBct1UcPSStUp4Xe1ii6EE114XyM41S65rjzBGU4akWVQYsdUlIZflok9r8kWV3OOp38kXFzMXW+suHCuUS6w+QyWbFnBUJdGBYya/+gpZ8LcudfQ9267A0SYk2uhKg2XpcwUUf6GomhQrtjlRVRMobwXvQHTMIR8i7M+AC7WIYIcYoEVyg3bCStdFlLivUICz+NV88heCqjDjk19ZUkgUyH4cyLNEJzsjPOXBlgs2+wny53AzeSzBzjrvmiq4bK5nBO48UfHBS4lMmjYx devops@WTLMLSFLAHERTY"
+# CHEF
+variable "chef_resource_group_name" {
+  default = "AZL-ChefServer-02"
 }
-variable "subscription_id" {
-  default = "9fbf7025-df40-4908-b7fb-a3a2144cee91"
+variable "chef_admin_password" {
+  default = "ZyADTVd64swkdvfHFMeR"
+}
+variable "chef_admin_user" {
+  default = "devops"
+}
+variable "chef_computer_name" {
+  default = "azl-chef-02"
+}
+# AUTOMATE 
+variable "auto_resource_group_name" {
+  default = "azl-auto-01"
+}
+variable "auto_admin_password" {
+  default = "ZyADTVd64swkdvfHFMeR"
+}
+variable "auto_admin_user" {
+  default = "devops"
+}
+variable "auto_computer_name" {
+  default = "azl-auto-01"
 }
